63e54c5a5375beed42afccf34f8019aebf0358b7
Thao Hoang <thaoh8198@gmail.com>
2024-02-16 22:07:06 +0700

    Xay dung layout chung

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 require('dotenv').config();
 const database = require("./config/database");
+
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
+
+const systemConfig = require("./config/system");
+
 database.connect();
+
 const app = express();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 app.use(express.static('public'));
 
 //Routes
