be23b01cdfe14b57c663337302d0bfba38621337
Thao Hoang <thaoh8198@gmail.com>
2024-02-16 23:04:01 +0700

    Lam giao dien danh danh san pham ben Admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,11 +1,15 @@
 const systemConfig = require("../../config/system")
 
 const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
+
 
 
 module.exports = (app) => {
     PATH_ADMIN = systemConfig.prefixAdmin;
-    app.use(PATH_ADMIN + "/dashboard",dashboardRoutes);
+    app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
+    app.use(PATH_ADMIN + "/products", productRoutes);
+
 
     
 }
