55c78973ce13a81d697897ad59eb816ddec07b74
thao0503 <thaohoanghust@gmail.com>
2024-02-21 13:06:12 +0700

    Tính năng tìm kiếm bên trang admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const searchHelpers = require("../../helpers/search")
 
 //[GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -31,15 +32,27 @@ module.exports.index = async (req, res) => {
     let find = {
         deleted: false
     };
+    // Lọc sản phẩm theo trạng thái
     if(req.query.status){
     find.status = req.query.status;
     }
+    //Kết thúc lọc sản phẩm theo trạng thái
+
+    //Tìm kiếm 
+    const objectSearch = searchHelpers(req.query);
+    if(objectSearch.keywordRegex){
+        find.title = objectSearch.keywordRegex; 
+    }
+    //Kết thúc tìm kiếm 
+
+
 
     const products = await Product.find(find);
 
     res.render("admin/pages/products/index.pug",{
         pageTitle: "Danh sách sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: objectSearch.keyword
     });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -17,3 +17,23 @@ if(buttonStatus.length > 0){
     })
 }
 // End Button status
+
+// Form search
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+    let url = new URL(window.location.href);
+    
+    formSearch.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        
+        if(keyword){
+            url.searchParams.set("keyword", keyword);
+        }else{
+            url.searchParams.delete("keyword");
+        }
+
+        window.location.href = url.href;
+    });
+}
+//  End Form search
