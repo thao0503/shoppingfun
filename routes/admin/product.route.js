bd2d3d58a1e8de487f66b2bfe857ae51e2f8ee61
thao0503 <thaohoanghust@gmail.com>
2024-03-04 23:11:39 +0700

    Tính năng thay đổi trạng thái sản phẩm

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/product.controller");
+
 router.get("/",controller.index);
+
+router.patch("/change-status/:status/:id",controller.changeStatus);
+
 module.exports = router;
