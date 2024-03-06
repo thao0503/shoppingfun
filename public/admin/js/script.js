60e1bc2ff26c11bbd0c09fa6e934e42444a4a8cb
thao0503 <thaohoanghust@gmail.com>
2024-03-06 23:39:48 +0700

    Tính năng thay đổi trạng thái nhiều sản phẩm

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,14 +1,14 @@
 // Button status
 const buttonStatus = document.querySelectorAll("[button-status]");
-if(buttonStatus.length > 0){
+if (buttonStatus.length > 0) {
     let url = new URL(window.location.href);
-    buttonStatus.forEach(button =>{
-        button.addEventListener("click",() => {
+    buttonStatus.forEach(button => {
+        button.addEventListener("click", () => {
             const status = button.getAttribute("button-status");
 
-            if(status){
+            if (status) {
                 url.searchParams.set("status", status);
-            }else{
+            } else {
                 url.searchParams.delete("status");
             }
 
@@ -20,16 +20,16 @@ if(buttonStatus.length > 0){
 
 // Form search
 const formSearch = document.querySelector("#form-search");
-if(formSearch){
+if (formSearch) {
     let url = new URL(window.location.href);
-    
-    formSearch.addEventListener("submit",(e)=>{
+
+    formSearch.addEventListener("submit", (e) => {
         e.preventDefault();
         const keyword = e.target.elements.keyword.value;
-        
-        if(keyword){
+
+        if (keyword) {
             url.searchParams.set("keyword", keyword);
-        }else{
+        } else {
             url.searchParams.delete("keyword");
         }
 
@@ -40,16 +40,80 @@ if(formSearch){
 
 // Pagination
 const buttonPagination = document.querySelectorAll("[button-pagination]");
-if(buttonPagination){
+if (buttonPagination) {
     let url = new URL(window.location.href);
     buttonPagination.forEach(button => {
-        button.addEventListener("click",() => {
+        button.addEventListener("click", () => {
             const page = button.getAttribute("button-pagination");
 
-            url.searchParams.set("page",page);
+            url.searchParams.set("page", page);
 
             window.location.href = url;
         })
     })
 }
 // End Pagination
+
+// Checkbox multi
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if (checkboxMulti) {
+    const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+    const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+
+    inputCheckAll.addEventListener("click", () => {
+        if (inputCheckAll.checked) {
+            inputsId.forEach(input => {
+                input.checked = true;
+            })
+        } else {
+            inputsId.forEach(input => {
+                input.checked = false;
+            })
+        }
+    })
+
+    
+    inputsId.forEach(input => {
+        input.addEventListener("click", () => {
+            const countChecked = checkboxMulti.querySelectorAll("input[name='id']:checked").length;
+
+            if(countChecked == inputsId.length){
+                inputCheckAll.checked = true;
+            }else{
+                inputCheckAll.checked = false;
+            }
+        })
+    })
+}
+
+
+// End Checkbox multi
+
+// Form change multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if(formChangeMulti){
+    formChangeMulti.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const checkboxMulti = document.querySelector("[checkbox-multi]");
+        const inputsIdChecked = checkboxMulti.querySelectorAll("input[name='id']:checked");
+
+        if(inputsIdChecked.length > 0){
+            let ids = [];
+            const inputIds = formChangeMulti.querySelector("input[name='ids']");
+
+            inputsIdChecked.forEach(input => {
+                const id = input.value;
+                ids.push(id);
+            });
+
+            inputIds.value = ids.join(", ");
+
+        }else{
+            alert("Vui lòng chọn ít nhất 1 bản ghi!")
+        }
+
+        formChangeMulti.submit();
+    })
+} 
+// End Form change multi
