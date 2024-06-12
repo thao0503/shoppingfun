faac6ffd989b784ea6ebbe4492de03db6a8f190b
thao0503 <thaohoanghust@gmail.com>
2024-06-12 23:00:57 +0700

    Tính năng upload ảnh

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const multer  = require('multer')
 const router = express.Router();
+const storageMulter = require(`../../helpers/storageMulter`);
+const upload = multer({ storage: storageMulter() })
 const controller = require("../../controllers/admin/product.controller");
 
 router.get("/",controller.index);
@@ -12,7 +15,7 @@ router.delete("/delete/:id",controller.deleteItem);
 
 router.get("/create",controller.create)
 
-router.post("/create",controller.createPost)
+router.post("/create",upload.single('thumbnail'),controller.createPost)
 
 
 module.exports = router;
