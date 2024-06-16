52e25f16049d20d0417102442e8eaa3c0e839452
thao0503 <thaohoanghust@gmail.com>
2024-06-16 21:58:59 +0700

    Tính năng sửa thông tin sản phẩm

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -21,5 +21,12 @@ router.post("/create",
     validate.createPost,
     controller.createPost)
 
+router.get("/edit/:id",controller.edit)
+
+router.patch("/edit/:id",
+    upload.single('thumbnail'),
+    validate.createPost,
+    controller.editPatch)
+
 
 module.exports = router;
