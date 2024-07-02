91492e78f9a580e80cca2e17019f5f954a98e458
thao0503 <thaohoanghust@gmail.com>
2024-07-02 22:33:03 +0700

    fix lại chức năng upload ảnh khi sửa thông tin sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -183,10 +183,6 @@ module.exports.editPatch = async(req, res) => {
     req.body.position = parseInt(req.body.position);
      
     
-    if(req.file){
-        req.body.thumbnail = `/uploads/${req.file.filename}`
-
-    }
 
     try {
         await Product.updateOne({ _id: id},req.body)
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -26,6 +26,7 @@ router.get("/edit/:id",controller.edit)
 
 router.patch("/edit/:id",
     upload.single('thumbnail'),
+    uploadCloud.uploadCloud,
     validate.createPost,
     controller.editPatch)
 
