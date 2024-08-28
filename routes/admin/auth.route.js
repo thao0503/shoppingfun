48e6dd47440611a4c1fe2e65b1fb96ba224efa28
thao0503 <thaohoanghust@gmail.com>
2024-08-28 21:23:31 +0700

    Tính năng đăng nhập

---
+++ routes/admin/auth.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const validate = require("../../validates/admin/auth.vadidate");
+const controller = require("../../controllers/admin/auth.controller");
+
+router.get("/login",controller.login);
+
+router.post("/login",validate.loginPost,controller.loginPost);
+
+module.exports = router;
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 
 
 
@@ -15,8 +16,7 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
     app.use(PATH_ADMIN + "/roles", roleRoutes);
     app.use(PATH_ADMIN + "/accounts", accountRoutes);
+    app.use(PATH_ADMIN + "/auth", authRoutes);
 
-
-    
 }
  
