a69a010eb0f51b39c6a20db64fe32eeb3b9d9855
thao0503 <thaohoanghust@gmail.com>
2024-09-05 17:10:37 +0700

    Hiển thị menu chung bên client

---
+++ middlewares/client/category.middleware.js
@@ -0,0 +1,13 @@
+const productCategory = require("../../models/product-category.model")
+const createTree = require("../../helpers/createTree")
+
+module.exports.category = async (req, res, next) => {
+    const categories = await productCategory.find({
+        deleted: false
+    });
+
+    const newCategories = createTree.tree(categories)
+
+    res.locals.categories = newCategories;
+    next();
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,11 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const categoryMiddleware = require("../../middlewares/client/category.middleware")
 
 
 module.exports = (app) => {
+    app.use(categoryMiddleware.category);
+
     app.use("/",homeRoutes);
 
     app.use("/products",productRoutes);
