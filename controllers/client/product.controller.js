7d42f4adc5dd01f3ed940223d41b84cacbaf9c35
thao0503 <thaohoanghust@gmail.com>
2024-09-21 17:06:10 +0700

    Phân trang bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,53 +1,77 @@
 const Product = require("../../models/product.model")
 const productCategory = require("../../models/product-category.model")
-const productsHelper = require("../../helpers/products") 
-const getAllSubCategoryIds = require("../../helpers/getAllSubCategoryIds") 
+const productsHelper = require("../../helpers/products")
+const getAllSubCategoryIds = require("../../helpers/getAllSubCategoryIds")
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /products
-module.exports.index = async (req,res) =>{
-    const products = await Product.find({
+module.exports.index = async (req, res) => {
+    const find = {
         status: "active",
         deleted: false
-    })
-    .sort({position: "desc"});
-    
-    const productsRepriced = productsHelper.newProductsPrice(products);
-      
+    }
 
-    res.render("client/pages/products/index.pug",{
+    // Phân trang
+    const countProducts = await Product.countDocuments(find);
+    let objectPagination = paginationHelper({
+            currentPage: 1,
+            limitItems: 12
+        },
+        req.query,
+        countProducts
+    )
+    // Kết thúc Phân trang
+
+
+    const products = await Product.find(find)
+        .sort({
+            position: "desc"
+        })
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);;
+
+    const productsRepriced = productsHelper.newProductsPrice(products);
+
+
+    res.render("client/pages/products/index.pug", {
         pageTitle: "Danh sách sản phẩm",
-        products: productsRepriced
+        products: productsRepriced,
+        pagination: objectPagination
     });
 }
 
 // [GET] /products/:categorySlug
-module.exports.category = async (req,res) =>{ 
+module.exports.category = async (req, res) => {
 
     try {
         const categorySlug = req.params.categorySlug;
-        
+
         const category = await productCategory.findOne({
             slug: categorySlug,
             status: "active",
             deleted: false
         });
-        
+
         //Lấy tất cả các danh mục con 
         const allCategories = await productCategory.find({
             deleted: false
         });
-        
+
         const allSubIds = getAllSubCategoryIds(allCategories, category.id, "active");
         // Kêt thúc lấy tất cả các danh mục con
 
         const products = await Product.find({
-            product_category_id: { $in: [category.id, ...allSubIds]},
+            product_category_id: {
+                $in: [category.id, ...allSubIds]
+            },
             deleted: false
-        }).sort({position: "desc"})
+        }).sort({
+            position: "desc"
+        })
 
         const productsRepriced = productsHelper.newProductsPrice(products);
 
-        res.render("client/pages/products/index.pug",{
+        res.render("client/pages/products/index.pug", {
             pageTitle: category.title,
             products: productsRepriced
         });
@@ -63,11 +87,11 @@ module.exports.detail = async (req, res) => {
             deleted: false,
             status: "active",
             slug: req.params.productSlug
-        } 
-    
+        }
+
         const product = await Product.findOne(find);
 
-        if(product.product_category_id){
+        if (product.product_category_id) {
             const category = await productCategory.findOne({
                 _id: product.product_category_id,
                 status: "active",
@@ -78,12 +102,12 @@ module.exports.detail = async (req, res) => {
 
         product.newPrice = productsHelper.newProductPrice(product);
 
-        res.render("client/pages/products/detail.pug",{
+        res.render("client/pages/products/detail.pug", {
             pageTitle: product.title,
             product: product
         });
     } catch (error) {
         res.redirect(`/products`)
     }
-    
+
 }
--- public/js/script.js
+++ public/js/script.js
@@ -27,3 +27,19 @@ if(formSearch){
     });
 };
 // End seatch
+
+// Pagination
+const buttonPagination = document.querySelectorAll("[button-pagination]");
+if (buttonPagination) {
+    let url = new URL(window.location.href);
+    buttonPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+
+            url.searchParams.set("page", page);
+
+            window.location.href = url;
+        });
+    });
+};
+// End Pagination
